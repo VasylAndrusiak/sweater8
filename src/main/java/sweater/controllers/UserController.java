375e9222a7572aa7dd74e32650fcd644ad6408a6
Vasyl Andrusiak <vasyl.andrusiak2017@gmail.com>
2019-11-23 17:36:10 +0200

    roles

--- src/main/java/sweater/controllers/UserController.java
+++ src/main/java/sweater/controllers/UserController.java
@@ -3,8 +3,16 @@ package sweater.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import sweater.domain.Role;
+import sweater.domain.User;
 import sweater.repos.UserRepo;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
@@ -13,4 +21,43 @@ public class UserController {
     @Autowired
     private UserRepo userRepo;
 
+    @GetMapping
+    public String userList(Model model){
+        model.addAttribute("users", userRepo.findAll());
+
+        return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user
+    ) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepo.save(user);
+
+        return "redirect:/user";
+    }
 }
--- src/main/resources/templates/userList.ftl
+++ src/main/resources/templates/userList.ftl
@@ -1,10 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
+<#import "parts/common.ftl" as c>
 
-</body>
-</html>
+<@c.page>
+List of users
+<table>
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Role</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <#list users as user>
+        <tr>
+            <td>${user.username}</td>
+            <td><#list user.roles as role>${role}<#sep>, </#list></td>
+            <td><a href="/user/${user.id}">edit</a></td>
+        </tr>
+    </#list>
+    </tbody>
+</table>
+</@c.page>
