637c35b5c6a34d533cfe63548db2cf753adab85b
Vasyl Andrusiak <vasyl.andrusiak2017@gmail.com>
2019-11-24 00:52:20 +0200

    files2

--- src/main/java/sweater/config/MvcConfig.java
+++ src/main/java/sweater/config/MvcConfig.java
@@ -20,6 +20,7 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/**")
                 .addResourceLocations("file:/" + uploadPath + "/");
-
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
--- src/main/resources/templates/parts/common.ftl
+++ src/main/resources/templates/parts/common.ftl
@@ -3,7 +3,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>SpringApp</title>
+    <link rel="stylesheet" href="/static/style.css">
 </head>
 <body>
     <#nested>
